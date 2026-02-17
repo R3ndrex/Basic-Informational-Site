0e23319a68492c1f39d8c4bb7e6480f47a2d049d
R3ndrex <yacholohchtoli@gmail.com>
2026-02-17 11:22:04 +0200

    add routing

--- server.js
+++ server.js
@@ -1,5 +1,43 @@
 const fs = require("fs");
-const https = require("https");
+const http = require("http");
 
-const serverInstance = https.createServer((req, res) => {});
-serverInstance.listen(3000, localhost);
+const serverInstance = http.createServer((req, res) => {
+    path = "./views/";
+    res.setHeader("Content-Type", "text/html");
+    switch (req.url) {
+        case "/": {
+            path += "index.html";
+            res.statusCode = 200;
+            break;
+        }
+        case "/contact": {
+            path += "contact-me.html";
+            res.statusCode = 200;
+            break;
+        }
+        case "/about": {
+            path += "about.html";
+            res.statusCode = 200;
+            break;
+        }
+        case "/contact-us": {
+            res.statusCode = 302;
+            res.setHeader("Location", "/contact");
+            break;
+        }
+        default: {
+            path += "404.html";
+            res.statusCode = 404;
+            break;
+        }
+    }
+    fs.readFile(path, (error, data) => {
+        if (error) {
+            console.error(error);
+            res.end();
+        } else {
+            res.end(data);
+        }
+    });
+});
+serverInstance.listen(3000);
